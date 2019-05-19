1c4c3fcb77d5fa1cc1da4d0bc972c7c6aad8214f
FrankTheTank <franklin@bitsmasher.net>
2019-05-19 10:24:59 -0400

    mv files to src dir

--- src/nola_con/MyMenu.h
+++ src/nola_con/MyMenu.h
@@ -27,10 +27,9 @@ class MyMenu{
     int ledpattern;
 
     String my_menu[4][5] = {
-      { "bling", "games", "network", "about", "" },
-      { "Cyber Police", "rainbow", "white", "gradient", "" },
-      { "Mesh", "Scanner", "Deauth", "", "" },
-      { "Burgess Pong", "", "", "", "" }
+      { "bling1", network", "about"},
+      { "Cyber Police", "rainbow", "white", "gradient"},
+      { "Mesh", "Scanner", "Deauth", "Empty"}
     };
                                
     void show_menu();
